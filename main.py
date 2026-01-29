f005cc14ebec8f02faf37f06896573903c4af5df
PremKumar829 <devikhusbu862@gmail.com>
2026-01-29 09:05:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-from flask import Flask
+import os
+from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return "Orde App Successfully Deployed 🚀"
+@app.route("/", methods=["GET", "POST"])
+def webhook():
+    return "Bot is running"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
